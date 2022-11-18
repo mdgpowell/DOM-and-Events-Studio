4084ecacecc176ac18acc3efc44f682a6f8a6cc3
Marcus Powell <marcuspowell@MacBook-Pro-4.local>
2022-11-17 19:34:21 -0600

    added events for 3 buttons

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 // console.log(window)
-//console.dir(document);
-
-// test
+console.dir(document);
 
 window.addEventListener("load", function() {
     let takeoff = document.getElementById("takeoff");
@@ -12,7 +10,22 @@ window.addEventListener("load", function() {
             document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
             document.getElementById("shuttleBackground").style.backgroundColor = "blue";
             document.getElementById("spaceShuttleHeight").innerHTML = Number(document.getElementById("spaceShuttleHeight").innerHTML)+10000;
-            
         }
     });
+    let land = document.getElementById("landing");
+    land.addEventListener("click", function() {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        document.getElementById("flightStatus").innerHTML = `The shuttle has landed.`;
+        document.getElementById("shuttleBackground").style.backgroundColor = "green";
+        document.getElementById("spaceShuttleHeight").innerHTML = 0;
+
+    });
+    let abortButton = document.getElementById("missionAbort");
+    abortButton.addEventListener("click", function() {
+       if (window.confirm("Confirm that you want to abort the mission")) {
+            document.getElementById("flightStatus").innerHTML = "Mission aborted.";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = 0;
+       }
+    }); 
 });
