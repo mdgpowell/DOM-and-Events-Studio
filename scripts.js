46ee1a29f2054ebd83a551b0e40a756b30c226a8
Marcus Powell <marcuspowell@MacBook-Pro-4.local>
2022-11-17 18:30:26 -0600

    added confirm to takeoff button

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+// console.log(window)
+console.dir(document);
+
+window.addEventListener("load", function() {
+    let takeoff = document.getElementById("takeoff");
+    takeoff.addEventListener("click", function() {
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
+    });
+});
